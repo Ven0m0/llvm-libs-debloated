51e48cc49686679fbb9e36b64982dff9bd98048b
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-07 12:36:58 +0100

    Update opus-mini.sh

--- opus-mini.sh
+++ opus-mini.sh
@@ -4,27 +4,18 @@ set -e
 
 get-pkgbuild
 cd "$BUILD_DIR"
-
 # debloat package, remove features that make the lib 5 MiB
-sed -i \
-	-e '/-D deep-plc=enabled/d' \
-	-e '/-D dred=enabled/d' \
-	-e '/-D osce=enabled/d' \
-	"$PKGBUILD"
-
+sed -i -e '/-D deep-plc=enabled/d' \
+  -e '/-D dred=enabled/d' -e '/-D osce=enabled/d' "$PKGBUILD"
 # skip tests since they take too long
 sed -i -e 's|meson test -C build|echo "skipped" #meson test -C build|' ./PKGBUILD
-
 cat "$PKGBUILD"
-
 # Do not build if version does not match with upstream
-if check-upstream-version; then
+if check-upstream-version >/dev/null 2&>1; then
 	makepkg -fs --noconfirm --skippgpcheck
 else
 	exit 0
 fi
-
-ls -la
 rm -fv ./*-docs-*.pkg.tar.* *-debug-*.pkg.tar.*
 mv -v ./"$PACKAGE"-*.pkg.tar."$EXT" ../"$PACKAGE"-mini-"$ARCH".pkg.tar."$EXT"
 cd ..
